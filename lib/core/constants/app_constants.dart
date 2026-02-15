d4880b3f71f6e16c86efdef42f767ad44767ee69
sanjeetaaryal26 <sanjeetaaryal26@gmail.com>
2026-02-15 13:10:10 +0545

    add: introduce shared app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,22 @@
+class AppConstants {
+  static const appName = 'Bite Finder';
+  static const highRatingThreshold = 4.0;
+  static const maxProfileItems = 8;
+
+  static const cuisines = [
+    'Nepali',
+    'Pizza',
+    'Cafe',
+    'Momo',
+    'Burgers',
+    'Indian',
+    'Thai',
+    'Japanese',
+  ];
+
+  static const sortOptions = [
+    'Top Rated',
+    'Nearest',
+    'Most Reviewed',
+  ];
+}
